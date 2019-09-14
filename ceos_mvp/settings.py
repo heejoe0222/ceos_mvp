50b4979603e1e68b8b680cc2388de7374164823a
hanqyu <peakjnut37@gmail.com>
2019-09-14 16:22:29 +0900

    add logging

--- ceos_mvp/settings.py
+++ ceos_mvp/settings.py
@@ -137,7 +137,7 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'console_debug_false', 'mail_admins'],
+            'handlers': ['console', 'console_debug_false'],
             'level': 'INFO',
         },
         'django.server': {
