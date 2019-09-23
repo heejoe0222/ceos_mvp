2f7ab7f5096aff528d57c6ebe8fcf9af501c5c9b
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-09-24 05:37:26 +0900

    html 및 css 초기 완성

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
-def example(request):
+def example(request): #프로젝트명으로 함수명 바꾸기
     if request.method == 'POST':
         email = request.POST['email']
         if email == '' or re.match(EMAIL_REGEX, email) is None:
@@ -20,4 +20,15 @@ def example(request):
 
     return render(request, 'core/example.html')
 
+def cellect(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if email == '' or re.match(EMAIL_REGEX, email) is None:
+            messages.error(request, "이메일 형식이 잘못되었습니다.\n다시 입력해주세요!")
+            return HttpResponseRedirect(request.path)
 
+        Email.objects.create(email=request.POST['email'], submitted_from=request.path)
+        messages.success(request, "이메일 전송 성공!")
+        return HttpResponseRedirect(request.path)
+
+    return render(request, 'core/cellect.html')
